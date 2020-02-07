9bace790c0cd46dda39df1ee9ecf472e6e8e7a3e
erictoman <ericto117@gmail.com>
2020-02-07 08:28:27 -0600

    DOne

---
+++ 3CM8MVCSTRUTS13-15/src/java/admin/Action1.java
@@ -0,0 +1,27 @@
+package admin;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+public class Action1 extends org.apache.struts.action.Action {
+
+    private static final String SUCCESS = "success";
+    private static final String FAIL = "fail";
+
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        
+        ActionForm1 forma=(ActionForm1)form;
+        if(forma.getId().equals("ruben")){
+            return mapping.findForward(SUCCESS);
+        }
+        else{
+            return mapping.findForward(FAIL);
+        }
+    }
+}
---
+++ 3CM8MVCSTRUTS13-15/src/java/admin/ActionForm1.java
@@ -0,0 +1,30 @@
+package admin;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+
+public class ActionForm1 extends org.apache.struts.action.ActionForm {
+    
+    private String id;
+    private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
