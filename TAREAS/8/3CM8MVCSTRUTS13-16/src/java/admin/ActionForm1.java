8d422e8ce4db384347f3eb822994a34278df63cd
Erictoman <ericto117@gmail.com>
2020-05-30 23:28:36 -0500

    Tareas added

---
+++ TAREAS/8/3CM8MVCSTRUTS13-16/src/java/admin/ActionForm1.java
@@ -0,0 +1,24 @@
+package admin;
+
+public class ActionForm1 extends org.apache.struts.action.ActionForm {
+    
+    private String id;
+    private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
